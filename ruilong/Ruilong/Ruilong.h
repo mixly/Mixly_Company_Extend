7c7d8717cc71d51fec05776197715e568a969e66
luokailiang <bnu_l@126.com>
2016-09-24 16:08:35 +0800

    update Ruilong Lib

--- ruilong/Ruilong/Ruilong.h
+++ ruilong/Ruilong/Ruilong.h
@@ -4,5 +4,7 @@
 #include "Arduino.h"
 #include "Adafruit_NeoPixel.h"  //for RGB
 #include "TM1650.h" //for 四位数码管
+#include "Adafruit_GFX.h" //for 88点阵
+#include "Adafruit_LEDBackpack.h" //for 88点阵
 
 #endif
