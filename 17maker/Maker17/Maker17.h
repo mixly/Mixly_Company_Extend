0018e4999f17b1d760c31587549147c0276d3b4d
hznupeter <hznupeter@hotmail.com>
2018-05-03 16:03:51 +0800

    调整库路径

--- 17maker/Maker17/Maker17.h
+++ 17maker/Maker17/Maker17.h
@@ -4,8 +4,15 @@
 #include <utility/Servo.h>
 #include <utility/EEPROM.h>
 #include "Arduino.h"
-#include "Adafruit_NeoPixel.h"  //for RGB
+#include "MaxMatrix.h.h"  //for RGB
 #include "Adafruit_GFX.h" //for 88点阵
 #include "Adafruit_LEDBackpack.h" //for 88点阵
+#include "u8g2.h"
+#include "U8g2lib.h"
+#include "u8x8.h"
+#include "U8x8lib.h"
+#include "SevenSegmentExtended.h"
+#include "SevenSegmentFun.h"
+#include "SevenSegmentTM1637.h"
 #endif
 
